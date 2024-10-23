16723c4b46de24026d2c1a013e964bf140f16b74
aliusmam <161744228+aliusmam@users.noreply.github.com>
2024-10-23 14:45:27 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,22 @@
 function replaceShortcodes() {
     const shortcodes = {
         fd: { name: '720p', regex: /\[fd id='(.*?)'\]/g, url: 'https://www.fastdrama.icu/p/' },
+        egf: { name: 'GDTOT', regex: /\[egt id='(.*?)'\]/g, url: 'https://new7.gdtot.dad/file/' },
         egf: { name: 'GDFLIX', regex: /\[egf id='(.*?)'\]/g, url: 'https://new5.gdflix.cfd/file/' },
-        egft: { name: 'GDTOT', regex: /\[egt id='(.*?)'\]/g, url: 'https://new7.gdtot.dad/file/' },
     };
 
-    document.querySelectorAll('.download-links').forEach(contentContainer => {
+    const contentContainers = document.querySelectorAll('.download-links');
+    contentContainers.forEach(contentContainer => {
         let content = contentContainer.innerHTML;
-
-        Object.values(shortcodes).forEach(({ name, regex, url }) => {
-            content = content.replace(regex, (match, id) => 
-                `<a href='${url}${id}' target='_blank'>${name}</a>`
-            );
-        });
-
+        for (const key in shortcodes) {
+            if (shortcodes.hasOwnProperty(key)) {
+                content = content.replace(shortcodes[key].regex, function(match, id) {
+                    return `<a href='${shortcodes[key].url}${id}' target='_blank' class="f-links">
+                                ${shortcodes[key].name}
+                            </a>`;
+                });
+            }
+        }
         contentContainer.innerHTML = content;
     });
 }
